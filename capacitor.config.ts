3b99759107d8863f1d09d0e4d13799a71c28a81f
CrCryptobyte <camiloruiz0r@gmail.com>
2026-06-23 23:09:34 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mimascotaspa.app',
+  appName: 'Mi Mascota Spa',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
